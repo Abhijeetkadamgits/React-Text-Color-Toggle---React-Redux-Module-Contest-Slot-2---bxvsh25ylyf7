89b6cc5f026c09150e9a05f0f8c5c9818d441d37
Abhijeetkadamgits <94745660+Abhijeetkadamgits@users.noreply.github.com>
2023-12-17 17:42:25 +0530

    App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,40 @@
-import React, {useEffect, useState} from 'react'
+// import React, {useEffect, useState} from 'react'
+// import '../styles/App.css';
+
+// const App = () => {
+// //code here 
+// const [isRed, setisRed] = useState(true);
+// const handleButtonClick = () => {
+// setIsRed((prebisRed) => ! prebisRed)
+// };
+//   return (
+//     <div id="main">
+//       <p className={isRed ? 'redText': 'blueText'} >Newton School</p>
+//       <button id='button' onClick={handleButtonClick}>Change Style</button>
+//     </div>
+//   )
+// }
+
+
+// export default App;
+import React, { useEffect, useState } from 'react';
 import '../styles/App.css';
 
 const App = () => {
-//code here 
+  // code here
+  const [isRed, setIsRed] = useState(true);
+
+  const handleButtonClick = () => {
+    setIsRed((prevIsRed) => !prevIsRed);
+  };
+
   return (
     <div id="main">
-      <p className={} >Newton School</p>
-      <button id='button' onClick={}>Change Style</button>
+      <p className={isRed ? 'redText' : 'blueText'}>Newton School</p>
+      <button id='button' onClick={handleButtonClick}>Change Style</button>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
+
